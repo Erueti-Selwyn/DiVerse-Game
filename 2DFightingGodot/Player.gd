e79bf4021e75bf15d0d6d927ee2a50e6156eb6f0
Erueti-Selwyn <eselwyn@students.mags.school.nz>
2024-06-24 09:40:09 +1200

    Minor Edits

--- 2DFightingGodot/Player.gd
+++ 2DFightingGodot/Player.gd
@@ -5,7 +5,7 @@ var speed = 325
 var normalSpeed = 325
 var crouchSpeed = 200
 
-var dashDirection = Vector2(1, 0)
+var dashDirection = Vector2(0, 0)
 var canDash = false
 var dashing = false
 var dashSpeed = 1500
@@ -34,13 +34,12 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	
 	var direction = Input.get_axis("move_left", "move_right")
-	
+	dashDirection = direction
 	if dashing:
-		velocity.x = direction * dashSpeed
+		velocity.x = dashDirection * dashSpeed
 	else:
-		if direction:
+		if direction and !dashing:
 			velocity.x = direction * speed
 		else:
 			velocity.x = move_toward(velocity.x, 0, speed)
@@ -51,13 +50,13 @@ func _physics_process(delta):
 	
 func crouch():
 	if Input.is_action_pressed("move_down"):
-		scale.y = 1.25
+		scale.y = -1.5
 		if is_on_floor():
 			speed = crouchSpeed
 		else:
 			speed = normalSpeed
 	else:
-		scale.y = 2
+		scale.y = -2
 		speed = normalSpeed
 		
 func dash():
