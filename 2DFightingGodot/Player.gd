cf20477fe024c6980c2231e018694c3b9b718b66
Erueti-Selwyn <eselwyn@students.mags.school.nz>
2024-06-27 10:20:10 +1200

    Update Player.gd

--- 2DFightingGodot/Player.gd
+++ 2DFightingGodot/Player.gd
@@ -52,26 +52,14 @@ func _physics_process(delta):
 			velocity.x = direction * speed
 		else:
 			velocity.x = move_toward(velocity.x, 0, speed)
-
-	move_and_slide()
-	crouch()
-	dash()
-	
-func crouch():
+			
 	if Input.is_action_pressed("move_down"):
-		scale.y = -1.5
 		crouching = true
-		if is_on_floor():
-			if !dashing:
-				speed = crouchSpeed
-		else:
-			if !dashing:
-				speed = normalSpeed
 	else:
 		crouching = false
-		if !dashing:
-			scale.y = -2
-			speed = normalSpeed
+
+	move_and_slide()
+	dash()
 		
 func dash():
 	if dashing:
