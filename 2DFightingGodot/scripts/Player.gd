b7973bab9ac07e6afd6a93f2940ba909bfc5d0e1
Erueti-Selwyn <eselwyn@students.mags.school.nz>
2024-08-06 13:28:05 +1200

    Minor Changes

--- 2DFightingGodot/scripts/Player.gd
+++ 2DFightingGodot/scripts/Player.gd
@@ -253,7 +253,7 @@ func _on_melee_body_entered(body):
 func is_hit():
 	health -= 10
 	isHit = true
-	_animated_sprite.modulate = Color(1, 0, 0)
+	_animated_sprite.modulate = Color(1, 0, 0) 
 	await get_tree().create_timer(0.15).timeout
 	isHit = false
 	_animated_sprite.modulate = Color(1, 1, 1)
